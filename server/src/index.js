633a0c88083d7fb0f98b33d1eea4737b1b8851c7
Kaleb <kaleb.coyo@ti.ukdw.ac.id>
2026-01-17 17:32:09 +0700

    setup deploy backend ke vercel

--- server/src/index.js
+++ server/src/index.js
@@ -6,12 +6,22 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server berjalan di port ${port}`);
+    });
+}
+
 app.use(cors()); // Izinkan Frontend mengakses Backend
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Backend is Running!'));
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server berjalan di port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server berjalan di port ${port}`);
+    });
+}
+
+module.exports = app;
