02d8b0e7410542bb33efa6af14982974e4651599
Kaleb <kaleb.coyo@ti.ukdw.ac.id>
2026-01-21 08:51:39 +0700

    tambah kirim email

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -71,7 +71,7 @@ export default function Home() {
                     Connect With Me
                 </div>
                 {isDropdownOpen && (
-                <div className="absolute group right-12 mt-32 w-48 bg-white border-2 border-black overflow-hidden flex flex-col">
+                <div className="absolute group right-14 mt-48 w-48 bg-white border-2 border-black overflow-hidden flex flex-col">
                     
                     {/* Link LinkedIn */}
                     <a 
@@ -90,11 +90,22 @@ export default function Home() {
                         href="https://github.com/kalebcwag"
                         target="_blank" 
                         rel="noopener noreferrer"
-                        className="px-4 py-3 text-black font-bold group-hover:text-gray-800 transition flex items-center gap-2"
+                        className="px-4 py-3 text-black font-bold hover:text-gray-500 transition flex items-center gap-2 border-b"
                     >
                         {/* Ikon GitHub Sederhana */}
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
                         GitHub
+                    </a>
+                    <a 
+                        href="https://mail.google.com/mail/?view=cm&fs=1&to=kalebcoyo@gmail.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="px-4 py-3 text-black font-bold hover:text-red-800 transition flex items-center gap-2 underline"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                        </svg>
+                        Email Me!
                     </a>
                 </div>
             )}
@@ -113,7 +124,7 @@ export default function Home() {
             <div className="text-center md:text-left flex-1">
                 <h1 className="text-3xl md:text-5xl font-extrabold text-black leading-tight">
                     Hello, World! <br className="hidden md:block"/> 
-                    <RoughNotation type="highlight" show={true} color="#fde047" padding={10} animationDelay={1000}>Kaleb's here.</RoughNotation>
+                    <RoughNotation type="highlight" show={true} color="#fde047" padding={10} animationDelay={1000}>Kaleb</RoughNotation>'s here.
                 </h1>
                 <p className="mt-6 text-base md:text-xl text-gray-700 leading-relaxed">
                     Informatics Graduate (2025) with a strong interest in <RoughNotation type="box" color="#ff0000" show={true} multiline={true}>AI and Data Science</RoughNotation>, also a strong foundation in <RoughNotation multiline={true} type="underline" color="#0000ff" show={true}>Software Engineering, Object-Oriented Programming (OOP), and Application Development.</RoughNotation> 
